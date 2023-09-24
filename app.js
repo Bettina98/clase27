a32185bcf298bf558b5fceb5b79ab89d8e4fbdd4
Bettina98 <beona67@gmail.com>
2023-09-24 02:33:35 -0300

    agrega funcionalidad restar

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 //todo: hacer el front de la calculadora
 const process = require('process');
+const restar = require('./restar')
 const {argv} = process;
 
-function calculadora(comando){
+function calculadora(comando, a, b){
   switch (comando) {
     case "sumar":
         
         break;
         case "restar":
-
+        return restar(a,b)
         break;
         case "multiplicar":
         
@@ -22,5 +23,5 @@ function calculadora(comando){
 }  
 }
 
-console.log(calculadora(argv[2], argv[3], argv[4]));
+console.log(calculadora(argv[2], +argv[3], +argv[4]));
 
